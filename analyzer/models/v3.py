e7417da83a3dd25722d922e961a589a862f0663c
Adam <adam@admlbs.fr>
2025-06-18 16:40:41 +0200

    Add comments to model creation functions for clarity

--- analyzer/models/v3.py
+++ analyzer/models/v3.py
@@ -4,10 +4,10 @@ from tensorflow.keras.layers import Dense, Dropout, LSTM, Conv1D, MaxPool1D, Fla
 
 def create_model(segment_size):
     model = Sequential()
-    model.add(Conv1D(segment_size * 2, kernel_size=3, input_shape=(segment_size, 5), activation='relu'))
+    model.add(Conv1D(segment_size * 2, kernel_size=3, input_shape=(segment_size, 5), activation='relu')) # Convolutional neural network layer
     model.add(MaxPool1D())
-    model.add(Flatten())
-    model.add(Dense(segment_size * 6, activation='relu'))
+    model.add(Flatten()) # Essential to compress all the data in the convolutional layer
+    model.add(Dense(segment_size * 6, activation='relu')) # Relu is selected to parametrize neuron behaviour and prevent linearity
     model.add(Dropout(0.2))
     model.add(Dense(segment_size * 2, activation='relu'))
     model.add(Dense(1, activation='sigmoid'))
